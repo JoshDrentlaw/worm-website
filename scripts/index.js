0bb1c2d925400db689c6ccb438ea81b97795d6e3
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-02-02 21:39:31 -0800

    Weekday script highlights day of the week in schedule.

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
 (function() {
-    
+    let weekday = new Date().getDay();
+    const weekdays = document.getElementById('weekdays').children;
+    weekdays.item(weekday).classList.add('bg-info', 'text-light');
 }());
