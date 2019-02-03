95b34b55fc4bb58803a5795f7d1bc39381b9d324
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-02-02 21:40:56 -0800

    Commented js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 (function() {
+    /* HIGHLIGHT DAY OF THE WEEK IN SCHEDULE */
     let weekday = new Date().getDay();
     const weekdays = document.getElementById('weekdays').children;
     weekdays.item(weekday).classList.add('bg-info', 'text-light');
