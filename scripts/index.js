edd563f0a2a3870076d807cc7c35ccc67699c25f
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-02-03 10:37:01 -0800

    Added Bootstrap form validation.

--- scripts/index.js
+++ scripts/index.js
@@ -3,4 +3,20 @@
     let weekday = new Date().getDay();
     const weekdays = document.getElementById('weekdays').children;
     weekdays.item(weekday).classList.add('bg-info', 'text-light');
+
+    /* DISABLE FORM SUBMISSION IF THERE ARE INVALID FIELDS */
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
 }());
