94b1268ec7e79fb206570858fa51d7c07cf05776
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-02-04 22:02:08 -0800

    Small UI adjustments. Navlinks.

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,14 @@
     /* HIGHLIGHT DAY OF THE WEEK IN SCHEDULE */
     let weekday = new Date().getDay();
     const weekdays = document.getElementById('weekdays').children;
-    weekdays.item(weekday).classList.add('bg-info', 'text-light');
+    weekdays.item(weekday).classList.add('buy-color', 'text-light');
 
     /* DISABLE FORM SUBMISSION IF THERE ARE INVALID FIELDS */
     window.addEventListener('load', function () {
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
+        let forms = document.getElementsByClassName('needs-validation');
         // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function (form) {
+        let validation = Array.prototype.filter.call(forms, function (form) {
             form.addEventListener('submit', function (event) {
                 if (form.checkValidity() === false) {
                     event.preventDefault();
@@ -19,4 +19,14 @@
             }, false);
         });
     }, false);
+
+    /* SET SVG ICON SIZES */
+    /* const desktop = window.matchMedia("(min-width: 992px)");
+    const svgs = document.getElementsByTagName('svg');
+    if (desktop.matches) {
+
+    }
+    else {
+        svgs.map
+    } */
 }());
