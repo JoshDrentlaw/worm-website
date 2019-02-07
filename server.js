d079ec7311e096ab914badd41f45fb21b6f338aa
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-02-06 17:14:47 -0800

    Use nodemailer for form.

---
+++ server.js
@@ -0,0 +1,39 @@
+"use strict";
+const nodemailer = require('nodemailer');
+const express = require('express');
+const server = express();
+
+server.post('/', (req, res) => {
+    main().catch(console.error);
+});
+
+async function main() {
+    const user = "joshdrentlaw@gmail.com";
+    const pass = "v.u5xQo0g";
+
+    // Create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: user,
+            pass: pass
+        }
+    });
+
+    // Setup email data with unicode symbols
+    let mailOptions = {
+        from: user,
+        to: user,
+        subject: "Test",
+        html: `<h1>TEST</h1>`
+    };
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail(mailOptions);
+
+    console.log("message sent: %s", info.messageId);
+}
+
+server.listen(5500, console.log('Server listening on port 5500....'));
