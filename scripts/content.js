e678c3c6d05abb4fa1da85f63b9ae932cc4845b9
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-02-23 15:48:10 -0800

    Added language state. Defaults to browser default. Need Spanish content.

---
+++ scripts/content.js
@@ -0,0 +1,58 @@
+'use strict';
+
+export default {
+    "en-US": {
+        "text": {
+            "home-link": "Home",
+            "about-link": "About",
+            "company-hdr": "Alcaraz<br />Worms and Compost",
+            "pitch": "Start your own compost system run by<br />WORM POWER",
+            "email-lbl": "Email",
+            "email-disclaimer": "I'll never share your email with anyone else.",
+            "worms-lbl": "Worms",
+            "worms-uom": "($10 per lb.)",
+            "worms-desc": "Every pound contains: Adult Earthworms, Juvenile Earthworms, Egg Cases, and Compost!",
+            "compost-lbl": "Vermicompost",
+            "compost-uom": "($50 per 10 lb.)",
+            "compost-desc": "Rich black compost castings. Some worms may be included!",
+            "tea-lbl": "Compost Tea",
+            "tea-uom": "($25 per 5 gal.)",
+            "tea-desc": "FRESH compost tea. Feed your plants with naturally high nutrient liquid fertilizer.",
+            "delivery-lbl": "Delivery",
+            "pickup-lbl": "Pickup",
+            "contact-btn": "Contact",
+            "delivery-hours-hdr": "Hours for Delivery"
+        },
+        "attr": {
+            "email-placeholder": "Enter email",
+            "comment-placeholder": "If you have any questions, concerns, or special instructions, let me know here."
+        }
+    },
+    "es": {
+        "text": {
+            "home-link": "",
+            "about-link": "",
+            "company-hdr": "",
+            "pitch": "",
+            "email-lbl": "",
+            "email-disclaimer": "",
+            "worms-lbl": "",
+            "worms-uom": "",
+            "worms-desc": "",
+            "compost-lbl": "",
+            "compost-uom": "",
+            "compost-desc": "",
+            "tea-lbl": "",
+            "tea-uom": "",
+            "tea-desc": "",
+            "delivery-lbl": "",
+            "pickup-lbl": "",
+            "contact-btn": "",
+            "delivery-hours-hdr": ""
+        },
+        "attr": {
+            "email-placeholder": "",
+            "comment-placeholder": ""
+        }
+    }
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,62 @@
-(function() {
-    /* HIGHLIGHT DAY OF THE WEEK IN SCHEDULE */
-    let weekday = new Date().getDay();
-    const weekdays = document.getElementById('weekdays').children;
-    weekdays.item(weekday).classList.add('buy-color', 'text-light');
+'use strict';
 
-    /* DISABLE FORM SUBMISSION IF THERE ARE INVALID FIELDS */
-    window.addEventListener('load', function () {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        let forms = document.getElementsByClassName('needs-validation');
-        // Loop over them and prevent submission
-        let validation = Array.prototype.filter.call(forms, function (form) {
-            form.addEventListener('submit', function (event) {
-                if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-                form.classList.add('was-validated');
-            }, false);
-        });
-    }, false);
+/* HIGHLIGHT DAY OF THE WEEK IN SCHEDULE */
+let weekday = new Date().getDay();
+const weekdays = document.getElementById('weekdays').children;
+weekdays.item(weekday).classList.add('buy-color', 'text-light');
 
-    /* SET SVG ICON SIZES */
-    /* const desktop = window.matchMedia("(min-width: 992px)");
-    const svgs = document.getElementsByTagName('svg');
-    if (desktop.matches) {
+/* DISABLE FORM SUBMISSION IF THERE ARE INVALID FIELDS */
+window.addEventListener('load', function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    let forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    let validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener('submit', function (event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+}, false);
 
+/* SWITCH CONTENT BETWEEN ENGLISH AND SPANISH */
+import Content from "./content.js";
+
+let lang = navigator.language;
+let html = document.getElementsByTagName('HTML')[0];
+html.setAttribute('lang', lang);
+
+let content = (lang == 'en-US') ? Content["en-US"] : Content.es;
+setContent(content);
+
+let langBtn = document.getElementById('lang-btn');
+langBtn.innerText = (lang == 'en-US') ? 'To Spanish' : 'To English';
+langBtn.addEventListener('click', e => {
+    if (html.getAttribute('lang') == 'en-US') {
+        langBtn.innerText = 'To English';
+        html.setAttribute('lang', 'es');
+        setContent(Content.es);
     }
     else {
-        svgs.map
-    } */
-}());
+        langBtn.innerText = 'To Spanish';
+        html.setAttribute('lang', 'en-US');
+        setContent(Content["en-US"]);
+    }
+});
+
+function setContent(content) {
+    /* SET INNER HTML */
+    for (let text in content.text) {
+        let el = document.getElementById(text);
+        el.innerHTML = content.text[text];
+    }
+
+    /* SET PLACEHOLDERS */
+    for (let attr in content.attr) {
+        let elId = attr.split('-')[0];
+        let el = document.getElementById(elId);
+        el.setAttribute('placeholder', content.attr[attr]);
+    }
+}
