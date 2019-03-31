6f94634e7b86267a0c4178666396e23ead6041e8
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-03-30 22:25:40 -0700

    Setup for Gatsby Netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/hours`,
+        name: "markdown-pages",
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -27,11 +35,8 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
     `gatsby-plugin-netlify`,
-    `gatsby-plugin-postcss`,
+    'gatsby-plugin-postcss',
     `gatsby-plugin-netlify-cms`,
     'gatsby-plugin-netlify-identity-widget'
   ],
