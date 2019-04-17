62e24e480eaacf8862f99ce209e083f38e331c1e
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-04-17 13:18:21 -0700

    Working on custom form submission.

--- src/components/form.js
+++ src/components/form.js
@@ -56,6 +56,50 @@ const encode = (data) => {
         .join('&');
 }
 
+const MainForm = (props) => (
+    <Container id="contact-form" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="contact" />
+        <Header>Want to get in touch?</Header>
+        <FormGroup>
+            <label htmlFor="worm-count" className="font-semibold">Worms</label>
+            <UnitInput type="number" min="0" step="1" defaultValue={props.values.worms} onChange={props.handleChange} name="worms" className="num-input" id="worm-count" aria-describedby="worms-uom" required />
+            <UnitDesc id="worms-uom">($10 per lb.)</UnitDesc>
+        </FormGroup>
+        <SmallInfo>Every pound contains: Adult Earthworms, Juvenile Earthworms, Egg Cases, and Compost!</SmallInfo>
+        <FormGroup>
+            <label htmlFor="compost-count" className="font-semibold">Vermicompost</label>
+            <UnitInput type="number" min="0" step="10" defaultValue={props.values.compost} onChange={props.handleChange} name="compost" className="num-input" id="compost-count" aria-describedby="compost-uom" required />
+            <UnitDesc id="compost-uom">($50 per 10 lb.)</UnitDesc>
+        </FormGroup>
+        <SmallInfo>Rich black compost castings. Some worms may be included!</SmallInfo>
+        <FormGroup>
+            <label htmlFor="compost-count" className="font-semibold">Compost Tea</label>
+            <UnitInput type="number" min="0" step="5" defaultValue={props.values.tea} onChange={props.handleChange} name="tea" className="num-input" id="tea-count" aria-describedby="tea-uom" required />
+            <UnitDesc id="tea-uom">($25 per 5 gal.)</UnitDesc>
+        </FormGroup>
+        <SmallInfo>FRESH compost tea. Feed your plants with naturally high nutrient liquid fertilizer.</SmallInfo>
+        <FormGroup>
+            <div className="mb-4">
+                <input type="radio" className="" id="delivery" name="shipment" defaultValue={props.values.delivery} onChange={props.handleChange} required />
+                <label htmlFor="delivery" className="font-semibold ml-1 mr-3">Delivery</label>
+            </div>
+            <div className="mb-4">
+                <input type="radio" className="" id="pickup" name="shipment" defaultValue={props.values.pickup} onChange={props.handleChange} defaultChecked required />
+                <label htmlFor="pickup" className="font-semibold ml-1">Pickup</label>
+            </div>
+        </FormGroup>
+        <FormGroup>
+            <textarea className="p-2 mb-4 w-full block border-2 border-black leading-none" name="comment" rows="4" onChange={props.handleChange} placeholder="If you have any questions, concerns, or special instructions, let me know here."></textarea>
+        </FormGroup>
+        <FormGroup>
+            <label htmlFor="email" className="font-semibold">Email</label>
+            <FormInput type="email" name="email" id="form-email" className="py-px text-5xl" onChange={props.handleChange} aria-describedby="disclaimer" placeholder="Enter email" required />
+        </FormGroup>
+        <SmallInfo id="disclaimer">We'll never share your email with anyone else.</SmallInfo>
+        <button type="submit" className="block mx-auto bg-buy text-white p-2 rounded" onClick={props.validate}>Contact</button>
+    </Container>
+)
+
 class Form extends React.Component {
     constructor(props) {
         super(props);
@@ -70,7 +114,8 @@ class Form extends React.Component {
             tea: 0,
             shipment: false,
             comment: '',
-            email: ''
+            email: '',
+            step: 1
         }
     }
 
@@ -83,67 +128,29 @@ class Form extends React.Component {
         }
     }
 
-    handleSubmit(e) {
-        fetch('/', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: encode({ 'form-name': 'contact', ...this.state })
-        })
-            .then(() => alert('success'))
-            .catch(error => alert(error));
-
-        e.preventDefault();
-    }
-
     handleChange(e) {
         this.setState({ [e.target.name]: e.target.value });
     }
 
+    /* nextStep() {
+        this.setState({step: step++})
+    } */
+
     render() {
         const { worms, compost, tea, delivery, pickup } = this.state;
-        return (
-            <Container id="contact-form" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
-                <input type="hidden" name="form-name" value="contact" />
-                <Header>Want to get in touch?</Header>
-                <FormGroup>
-                    <label htmlFor="worm-count" className="font-semibold">Worms</label>
-                    <UnitInput type="number" min="0" step="1" defaultValue={worms} onChange={this.handleChange} name="worms" className="num-input" id="worm-count" aria-describedby="worms-uom" required />
-                    <UnitDesc id="worms-uom">($10 per lb.)</UnitDesc>
-                </FormGroup>
-                <SmallInfo>Every pound contains: Adult Earthworms, Juvenile Earthworms, Egg Cases, and Compost!</SmallInfo>
-                <FormGroup>
-                    <label htmlFor="compost-count" className="font-semibold">Vermicompost</label>
-                    <UnitInput type="number" min="0" step="10" defaultValue={compost} onChange={this.handleChange} name="compost" className="num-input" id="compost-count" aria-describedby="compost-uom" required />
-                    <UnitDesc id="compost-uom">($50 per 10 lb.)</UnitDesc>
-                </FormGroup>
-                <SmallInfo>Rich black compost castings. Some worms may be included!</SmallInfo>
-                <FormGroup>
-                    <label htmlFor="compost-count" className="font-semibold">Compost Tea</label>
-                    <UnitInput type="number" min="0" step="5" defaultValue={tea} onChange={this.handleChange} name="tea" className="num-input" id="tea-count" aria-describedby="tea-uom" required />
-                    <UnitDesc id="tea-uom">($25 per 5 gal.)</UnitDesc>
-                </FormGroup>
-                <SmallInfo>FRESH compost tea. Feed your plants with naturally high nutrient liquid fertilizer.</SmallInfo>
-                <FormGroup>
-                    <div className="mb-4">
-                        <input type="radio" className="" id="delivery" name="shipment" defaultValue={delivery} onChange={this.handleChange} required />
-                        <label htmlFor="delivery" className="font-semibold ml-1 mr-3">Delivery</label>
-                    </div>
-                    <div className="mb-4">
-                        <input type="radio" className="" id="pickup" name="shipment" defaultValue={pickup} onChange={this.handleChange} defaultChecked required />
-                        <label htmlFor="pickup" className="font-semibold ml-1">Pickup</label>
-                    </div>
-                </FormGroup>
-                <FormGroup>
-                    <textarea className="p-2 mb-4 w-full block border-2 border-black leading-none" name="comment" rows="4" onChange={this.handleChange} placeholder="If you have any questions, concerns, or special instructions, let me know here."></textarea>
-                </FormGroup>
-                <FormGroup>
-                    <label htmlFor="email" className="font-semibold">Email</label>
-                    <FormInput type="email" name="email" id="form-email" className="py-px text-5xl" onChange={this.handleChange} aria-describedby="disclaimer" placeholder="Enter email" required />
-                </FormGroup>
-                <SmallInfo id="disclaimer">We'll never share your email with anyone else.</SmallInfo>
-                <button type="submit" className="block mx-auto bg-buy text-white p-2 rounded" onClick={this.validate}>Contact</button>
-            </Container>
-        )
+        const values = { worms, compost, tea, delivery, pickup };
+        switch (this.state.step) {
+            case 1:
+                return (
+                    <MainForm 
+                        values={values}
+                        handleChange={this.handleChange}
+                        validate={this.validate}
+                    />
+                )
+            case 2:
+                return (<h2>Submitted</h2>)
+        }
     }
 }
 
