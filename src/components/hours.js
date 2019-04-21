46ae526934bf3fed4d8539104175c6315ca4dedd
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-04-21 16:25:32 -0700

    Hours move to another line on smaller screens.

--- src/components/hours.js
+++ src/components/hours.js
@@ -10,24 +10,35 @@ const Container = styled.section`
 `
 
 const Hours = ({ data }) => {
-    console.log(data)
-    useEffect(() => {
+
+    const highlightToday = () => {
         let weekday = new Date().getDay();
         const weekdays = document.getElementById('weekdays').children;
         weekdays.item(weekday).classList.add('bg-buy', 'text-white');
-    }) 
+    }
+
+    const getScreenSize = () => {
+        if (window.outerWidth <= 420) {
+            return (<br />);
+        }
+        else return null 
+    }
+
+    useEffect(() => {
+        highlightToday();
+    })
 
     return (
         <Container id="hours">
             <h2 className="border-b border-grey-dark font-hand text-9xl leading-hand p-4 pb-6">Hours for Delivery</h2>
             <ul className="list-reset text-2xl" id="weekdays">
-                <li className="border-b border-grey-dark py-2" id="sunday">Sunday: <span>{ data.allMarkdownRemark.edges[0].node.frontmatter.sunday }</span></li>
-                <li className="border-b border-grey-dark py-2" id="monday">Monday: <span className="hours">{ data.allMarkdownRemark.edges[0].node.frontmatter.monday }</span></li>
-                <li className="border-b border-grey-dark py-2" id="tuesday">Tuesday: <span className="hours">{ data.allMarkdownRemark.edges[0].node.frontmatter.tuesday }</span></li>
-                <li className="border-b border-grey-dark py-2" id="wednesday">Wednesday: <span className="hours">{ data.allMarkdownRemark.edges[0].node.frontmatter.wednesday }</span></li>
-                <li className="border-b border-grey-dark py-2" id="thursday">Thursday: <span className="hours">{ data.allMarkdownRemark.edges[0].node.frontmatter.thursday }</span></li>
-                <li className="border-b border-grey-dark py-2" id="friday">Friday: <span className="hours">{ data.allMarkdownRemark.edges[0].node.frontmatter.friday }</span></li>
-                <li className="py-2" id="saturday">Saturday: <span className="hours">{ data.allMarkdownRemark.edges[0].node.frontmatter.saturday }</span></li>
+                <li className="border-b border-grey-dark py-2" id="sunday">Sunday: {getScreenSize()} <span>{ data.allMarkdownRemark.edges[0].node.frontmatter.sunday }</span></li>
+                <li className="border-b border-grey-dark py-2" id="monday">Monday: {getScreenSize()} <span className="hours">{ data.allMarkdownRemark.edges[0].node.frontmatter.monday }</span></li>
+                <li className="border-b border-grey-dark py-2" id="tuesday">Tuesday: {getScreenSize()} <span className="hours">{ data.allMarkdownRemark.edges[0].node.frontmatter.tuesday }</span></li>
+                <li className="border-b border-grey-dark py-2" id="wednesday">Wednesday: {getScreenSize()} <span className="hours">{ data.allMarkdownRemark.edges[0].node.frontmatter.wednesday }</span></li>
+                <li className="border-b border-grey-dark py-2" id="thursday">Thursday: {getScreenSize()} <span className="hours">{ data.allMarkdownRemark.edges[0].node.frontmatter.thursday }</span></li>
+                <li className="border-b border-grey-dark py-2" id="friday">Friday: {getScreenSize()} <span className="hours">{ data.allMarkdownRemark.edges[0].node.frontmatter.friday }</span></li>
+                <li className="py-2" id="saturday">Saturday: {getScreenSize()} <span className="hours">{ data.allMarkdownRemark.edges[0].node.frontmatter.saturday }</span></li>
             </ul>
         </Container>
     )
